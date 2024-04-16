be0c6be870787a42419133443d6eb32fd2687925
aaron-agustin <aaron.agustin@laplateforme.io>
2024-04-16 15:08:42 +0200

    ajouts du local storage

--- decors.js
+++ decors.js
@@ -3,5 +3,12 @@ var cursors = 0;
 
 function scoreClick1() {
     points++;
-    document.getElementById("score").innerHTML = points; // Update points display
+    document.getElementById("score").innerHTML = points; 
+    saveClickCount();
 }
+
+
+function saveClickCount() {
+    localStorage.setItem("points", points);
+}
+
