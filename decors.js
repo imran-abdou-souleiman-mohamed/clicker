24a1c449e83eb2f9b43bb8e4d8edc08d984471c5
aaron-agustin <aaron.agustin@laplateforme.io>
2024-04-16 13:54:34 +0200

    ajouts dun contenaire contenant un mini tutoriel jai egalement commencer a modifier le bouton getpoints pour le rendre plus agréable a regarder

--- decors.js
+++ decors.js
@@ -5,18 +5,3 @@ function scoreClick1() {
     points++;
     document.getElementById("score").innerHTML = points; // Update points display
 }
-
-function buyItem() {
-    var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
-    if (points >= cursorCost) {
-        cursors++;
-        points -= cursorCost;
-        document.getElementById("cursors").innerHTML = cursors;
-        document.getElementById("score").innerHTML = points; // Update points display
-    }
-    else {
-        alert("You don't have enough points!");
-    }
-    var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
-    document.getElementById("cursorCost").innerHTML = nextCost;
-}
