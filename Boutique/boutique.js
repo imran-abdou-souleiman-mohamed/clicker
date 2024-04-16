509aa77ba8d592782c3e208cd1385efbf413ff01
imran-abdou-souleiman-mohamed <imran.abdou-souleiman-mohamed@laplateforme.io>
2024-04-16 12:28:33 +0200

    ajout du premier bouton de buff

--- Boutique/boutique.js
+++ Boutique/boutique.js
@@ -1,22 +1,42 @@
 var points = 0;
 var cursors = 0;
+var bufs = 0;
 
 function scoreClick1() {
     points++;
     document.getElementById("score").innerHTML = points; // Update points display
 }
 
-function buyItem() {
-    var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
-    if (points >= cursorCost) {
-        cursors++;
-        points -= cursorCost;
-        document.getElementById("cursors").innerHTML = cursors;
-        document.getElementById("score").innerHTML = points; // Update points display
+function buyItem(itemType) {
+    if (itemType === 'cursor') {
+        var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+        if (points >= cursorCost) {
+            cursors++;
+            points -= cursorCost;
+            document.getElementById("cursors").innerHTML = cursors;
+            document.getElementById("score").innerHTML = points; // Update points display
+        } else {
+            alert("You don't have enough points!");
+        }
+        var nextCursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+        document.getElementById("cursorCost").innerHTML = nextCursorCost;
+    } else if (itemType === 'buf') {
+        var bufCost = Math.floor(50 * Math.pow(1.1, bufs));
+        if (points >= bufCost) {
+            bufs++;
+            points -= bufCost;
+            document.getElementById("bufs").innerHTML = bufs;
+            document.getElementById("score").innerHTML = points; // Update points display
+            document.getElementById("points").setAttribute("onclick", "scoreClick3()");
+        } else {
+            alert("You don't have enough points!");
+        }
+        var nextBufCost = Math.floor(50 * Math.pow(1.1, bufs));
+        document.getElementById("bufCost").innerHTML = nextBufCost;
     }
-    else {
-        alert("You don't have enough points!");
-    }
-    var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
-    document.getElementById("cursorCost").innerHTML = nextCost;
+}
+
+function scoreClick3() {
+    points += 3;
+    document.getElementById("score").innerHTML = points; // Update points display
 }
